4b6efbdebb86f8ad6fb27154eb91e863a5c6fc97
Bernardo Domeneghetti <bernardodomeneghetti@gmail.com>
2026-03-25 00:39:14 -0300

    EmailAddresAttribute

--- src/Concierge.Net.Domain/Shared/Email.cs
+++ src/Concierge.Net.Domain/Shared/Email.cs
@@ -1,10 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using Concierge.Net.Domain.Base;
 
 namespace Concierge.Net.Domain.Shared;
 
 public sealed class Email(string value) : ValueObject
 {
+    [EmailAddress]
     public string Value { get; } = value 
         ?? throw new ArgumentNullException(nameof(value), "No value has been provided for e-mail value."); //TODO: Remove exception messages from hardCoded strings
 
